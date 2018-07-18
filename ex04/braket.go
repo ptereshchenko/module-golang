80d0b6a2e03d23de7de24d02f207defc723ddf0b
Polina <polatka3000@gmail.com>
2018-07-18 16:17:04 +0300

    corrected

--- ex04/braket.go
+++ ex04/braket.go
@@ -1,20 +1,20 @@
 package brackets
 
-var brackets = map[string]string{
-	"{": "}",
-	"[": "]",
-	"(": ")",
-}
+import "strings"
 
-func Bracket(input string) (bool, error) {
-	result := "_"
-	for _, c := range input {
-		value := brackets[result[len(result)-1:]]
-		result += string(c)
-		if string(c) == value {
-			result = result[:len(result)-2]
+func Bracket(str string) (bool, error) {
+	type1, type2, type3 := "()", "[]", "{}"
+
+	flag := true
+
+	for flag {
+		if strings.Contains(str, type1) || strings.Contains(str, type2) || strings.Contains(str, type3) {
+			str = strings.Replace(strings.Replace(strings.Replace(str, type1, "", -1), type2, "", -1), type3, "", -1)
+		} else if str == "" {
+			return flag, nil
+		} else {
+			flag = false
 		}
 	}
-	return len(result) == 1, nil
+	return flag, nil
 }
-
