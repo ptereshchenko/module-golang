90bffc7c575542cd116a3acb812d23ec45764d86
Polina <polatka3000@gmail.com>
2018-07-18 15:49:25 +0300

    corrected

--- ex04/braket.go
+++ ex04/braket.go
@@ -1,39 +1,20 @@
 package brackets
 
-func Bracket(str string) (bool, error) {
-  var a, b, c bool  = true, true, true
-
-  for i := 0; i < len(str); i++ {
-	if str[i] == '(' {
-      a = false
-	  for j := i + 1; j < len(str); j += 2 {
-		if str[j] == ')' {
-		  a = true
-		  break
-		}
-	  }
-	}
-	if str[i] == '[' {
-	  b = false
-	  for j := i + 1; j < len(str); j += 2 {
-		if str[j] == ']' {
-		  b = true
-		  break
-		}
-      }
-	}
-	if str[i] == '{' {
-	  c = false
-	  for j := i + 1; j < len(str); j += 2 {
-		if str[j] == '}' {
-		  c = true
-		  break
-		}
-	  }
-	}
-  }
-  if a && b && c {
-    return true, nil
-  }
-  return false, nil
+var brackets = map[string]string{
+	"{": "}",
+	"[": "]",
+	"(": ")",
 }
+
+func Bracket(input string) (bool, error) {
+	result := "_"
+	for _, c := range input {
+		value := brackets[result[len(result)-1:]]
+		result += string(c)
+		if string(c) == value {
+			result = result[:len(result)-2]
+		}
+	}
+	return len(result) == 1, nil
+}
+
